2748c2d96fa43384076684f775fb944060cd3cb4
blacktunes <blacktunes@foxmail.com>
2024-05-16 00:04:50 +0800

    feat: Confirm组件增加关闭事件

--- src/components/popup/Confirm/data.ts
+++ src/components/popup/Confirm/data.ts
@@ -3,6 +3,7 @@ export const data = reactive<{
   tip?: string
   text: string[]
   fn?: () => void
+  close?: () => void
 }>({
   title: '',
   tip: undefined,
@@ -12,17 +13,22 @@ export const data = reactive<{
 
 let confirm = () => {}
 export const callback = {
-  open: (config: { title: string; tip?: string; text: string[]; fn?: () => void }) => {
+  open: (config: typeof data) => {
     data.title = config.title
     data.tip = config.tip
     data.text = config.text
     data.fn = config.fn
+    data.close = config.close
   },
   close: () => {
     data.title = ''
     data.tip = undefined
     data.text = []
     data.fn = undefined
+    if (data.close) {
+      data.close()
+      data.close = undefined
+    }
   },
   set confirm(fn: () => any) {
     confirm = fn
