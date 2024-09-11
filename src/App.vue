c5ab586a3f4ab07089ca3fae7a96d8e1b1bbb432
blacktunes <blacktunes@foxmail.com>
2024-09-11 17:36:38 +0800

    feat: 添加组件测试预览

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,37 @@
 <template>
-  <div></div>
+  <Main
+    :viewport="{
+      width: 3200,
+      bottom: 100
+    }"
+    :popup="popupManager"
+    @click="click"
+  >
+  </Main>
 </template>
 
 <script lang="ts" setup>
+import { createPopupManager, Main, confirm } from '.'
 
+const popupManager = createPopupManager({
+  confirm
+})
+
+const click = () => {
+  popupManager.open('confirm', {
+    title: '测试',
+    text: ['测试文本']
+  })
+}
 </script>
 
-<style lang="stylus" scoped>
-
+<style lang="stylus">
+body
+  margin 0
+  background:
+    linear-gradient(217deg, rgba(255, 0, 0, 0.8), rgba(255, 0, 0, 0) 70.71%),
+    linear-gradient(127deg, rgba(0, 255, 0, 0.8), rgba(0, 255, 0, 0) 70.71%),
+    linear-gradient(336deg, rgba(0, 0, 255, 0.8), rgba(0, 0, 255, 0) 70.71%)
 </style>
+
+<style lang="stylus" scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,3 @@
-import './main.styl'
-
-import '@/assets/scripts/window'
-import './assets/scripts/analytics'
-import './assets/scripts/setup'
-import './assets/scripts/keyboard'
-
 import App from './App.vue'
-import VueDOMPurifyHTML from 'vue-dompurify-html'
 
-window.BUILD_TIME = new Date(BUILD_TIME)
-
-createApp(App)
-  .use(VueDOMPurifyHTML, {
-    allowedTags: ['br', 'span'],
-    allowedAttributes: {
-      span: ['style', 'class']
-    }
-  })
-  .mount('#app')
+createApp(App).mount('#app')
