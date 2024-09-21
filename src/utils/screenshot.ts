497c06924be9fb0d9470d7e13b70488830484c22
blacktunes <blacktunes@foxmail.com>
2024-09-21 21:31:25 +0800

    feat: 截图功能增加隐藏文件

--- src/utils/screenshot.ts
+++ src/utils/screenshot.ts
@@ -1,5 +1,20 @@
-import { getFontEmbedCSS, toPng } from 'html-to-image'
+import { getFontEmbedCSS, toBlob } from 'html-to-image'
 import type { Options } from 'html-to-image/lib/types'
+import JSzip from 'jszip'
+import { compress } from 'lz-string'
+
+const blobToBase64 = (blob: Blob) => {
+  return new Promise<string>((resolve, reject) => {
+    const reader = new FileReader()
+    reader.onloadend = () => {
+      resolve(reader.result as string)
+    }
+    reader.onerror = (error) => {
+      reject(error)
+    }
+    reader.readAsDataURL(blob)
+  })
+}
 
 export const screenshot = async (
   dom: HTMLElement,
@@ -8,25 +23,44 @@ export const screenshot = async (
     width?: number
     height?: number
     download?: boolean
+    data?: {
+      raw: string
+      filename: string
+    }
   },
   options: Options = {}
 ) => {
   try {
     const fontEmbedCSS = await getFontEmbedCSS(dom)
     const title = `${config?.name ? `${config.name} - ` : ''}${new Date().toLocaleString()}`
-    const dataUrl = await toPng(dom, {
+    const imageBlob = await toBlob(dom, {
       width: config?.width,
       height: config?.height,
       pixelRatio: 1,
       fontEmbedCSS,
       ...options
     })
+
+    if (!imageBlob) return Promise.reject(new Error('截图失败'))
+
+    const zip = new JSzip()
+    if (config?.data) {
+      const str = compress(config.data.raw)
+      zip.file(config.data.filename, str)
+    }
+    zip.file('blacktunes', 'https://github.com/blacktunes')
+    const zipBlob = await zip.generateAsync({ type: 'blob' })
+    const mergedBlob = new Blob([imageBlob, zipBlob], { type: 'image/png' })
+
     if (config?.download) {
+      const url = URL.createObjectURL(mergedBlob)
       const link = document.createElement('a')
       link.download = `${title}.png`
-      link.href = dataUrl
+      link.href = url
       link.click()
+      URL.revokeObjectURL(url)
     } else {
+      const dataUrl = await blobToBase64(mergedBlob)
       const img = new Image()
       img.src = dataUrl
       img.alt = title
